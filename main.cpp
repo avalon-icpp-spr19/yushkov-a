aa573195b65c38174ea5ea31a0ee137443c15a80
yushkov-i <47275068+yushkov-i@users.noreply.github.com>
2019-02-03 12:55:09 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+const float PI = 3.1415;
+
+int main()
+{
+	cout << numeric_limits<char>::digits << endl;
+	cout << numeric_limits<unsigned char>::digits << endl;
+
+
+}
